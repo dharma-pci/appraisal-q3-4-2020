a43ef5bd0d7f29210f7629d84d0c38fa4589ccf0
Alfif from Port Cities <alfif@portcities.net>
2021-01-19 23:52:03 +0700

    [FIX] bugs when route in shipping empty

--- pcs_website_delivery_options/controllers/website_sale_delivery.py
+++ pcs_website_delivery_options/controllers/website_sale_delivery.py
@@ -11,8 +11,9 @@ class WebsiteSaleDelivery(WebsiteSale):
         res = super(WebsiteSaleDelivery, self).update_eshop_carrier(**post)
         order = request.website.sale_get_order()
         carrier = request.env['delivery.carrier'].search([('id','=',post.get('carrier_id'))])
-        for line in order.order_line:
-            line.update({
-                'route_id':carrier.route_id.id
-            })
+        if carrier.route_id :
+            for line in order.order_line:
+                line.update({
+                    'route_id':carrier.route_id.id
+                })
         return res
