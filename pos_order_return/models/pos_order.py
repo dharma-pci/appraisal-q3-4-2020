ea5f776f59036df8ef674b7cd8fcefb7d31d03c7
andrew <andrew@portcities.net>
2021-01-20 17:38:21 +0700

    [IMP] Return order will prevent if product has been returned

--- pos_order_return/models/pos_order.py
+++ pos_order_return/models/pos_order.py
@@ -6,6 +6,7 @@ from odoo.tools.profiler import profile
 class PosOrder(models.Model):
     _inherit = "pos.order"
 
+    pos_return_from = fields.Many2one('pos.order', string='Return From')
     # get order that search by name or pos reference and must total not lower than zero 
     # this function will be called in js pos screen
     [...]
@@ -29,4 +30,35 @@ class PosOrder(models.Model):
             return orderlines
         else:
             return False
+
+    # make each coupon is consume each used
+    [...]
+    @profile
+    @api.model
+    def _process_order(self, order, draft, existing_order):
+        res = super(PosOrder, self)._process_order(order, draft, existing_order)
+        # created pos order will check each orderline coupon_id
+        new_order = order['data']
+        lines = new_order.get('lines')
+        for line in lines:
+            if line[2].get('from_return_line_id'):
+                order_line = self.env['pos.order.line'].browse(line[2].get('from_return_line_id'))
+                order_line.sudo().write({'qty_return':(line[2].get('qty') * -1),}) 
+        return res
+
+    [...]
+    @profile
+    @api.model
+    def _order_fields(self, ui_order):
+        res = super(PosOrder, self)._order_fields(ui_order)
+        if ui_order.get('pos_return_from', False):
+            res.update({
+                'pos_return_from': ui_order['pos_return_from']
+            })
+        return res
             
+class PosOrderLine(models.Model):
+    _inherit = "pos.order.line"
+
+    from_return_line_id = fields.Many2one('pos.order.line', string="Return From Pos Line")
+    qty_return = fields.Float(string='Qty Returned')
